4df3dd5fde43b6c41c33b4b15a042bc070c8e1e7
somyeong04 <144991811+somyeong04@users.noreply.github.com>
2023-11-05 14:43:56 +0900

    3-2

--- main.c
+++ main.c
@@ -5,20 +5,23 @@
 #define N_STUDENT 5
 
 int main(int argc, char *argv[]) {
-	int grade[N_STUDENT];
-	int score[N_STUDENT];
+	int a[N_STUDENT] = {1, 2, 3, 4, 5};
+	int b[N_STUDENT] = {1, 2, 3, 4, 6};
 	int i;
+	int flag = 0;
 	
-	for (i=0; i<N_STUDENT; i++) 
-		grade[i] = rand()%100 + 1;
+	for (i=0; i<N_STUDENT; i++)
+	{
+		if (a[i]!=b[i])
+		{
+			printf("array a and b are not the same\n");
+			flag = 1;
+		}
+	}
 	
-	
-	for (i=0; i<N_STUDENT; i++) 
-		score[i] = grade [i];
-		
-	for (i=0; i<N_STUDENT; i++) 
-		printf("score[%d] = %d (grade: %i)\n", i, score[i], grade[i]);
-	
+	if(flag == 0)
+		printf("array a and b are the same\n");	
+
 	system("PAUSE");
 	return 0;
 }
